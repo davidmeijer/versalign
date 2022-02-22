b17c547c8508fa4bfe1637746749777a7ed59a2b
davidmeijer <david-meijer@live.nl>
2022-02-23 00:22:11 +0100

    OOPS: typo

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ./run_multiple_sequence_alignment.py --fasta ../data/pk_test.fasta --scores ../scoring_matrices/scoring_matrix_similarity.txt > ../_out/msa_similarity.msa
 ./run_multiple_sequence_alignment.py --fasta ../data/pk_test.fasta --scores ../scoring_matrices/scoring_matrix_match_mismatch.txt > ../_out/msa_match_mismatch.msa
-./make_logo_from_msa.py ../_out/msa_match_mismatch.msa --out ../_out/logo_match_mismatch2.png
-./make_logo_from_msa.py ../_out/msa_similarity.msa --out ../_out/logo_similarity2.png
+./make_logo_from_msa.py ../_out/msa_match_mismatch.msa --out ../_out/logo_match_mismatch.png
+./make_logo_from_msa.py ../_out/msa_similarity.msa --out ../_out/logo_similarity.png
 
